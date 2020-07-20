01a14054bf62fd1e3d635193e2edc7c5f1b80668
takeshi furusawa <djpdjp0517@gmail.com>
2020-07-20 09:27:36 +0000

    投稿機能追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,9 +3,20 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @blog = Blog.new(blog_params)
+    if params[:back]
+      render :new
+    else
+      if @blog.save
+        redirect_to blogs_path, notice:"ブログが追加されました。"
+      else
+        render :new
+      end
+    end
   end
 
   def edit
@@ -18,6 +29,11 @@ class PostsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:content)
   end
 
 end
