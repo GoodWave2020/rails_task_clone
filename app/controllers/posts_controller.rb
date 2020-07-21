14853c397721d84637e72ea65b94149728eba0f1
takeshi furusawa <djpdjp0517@gmail.com>
2020-07-21 01:57:31 +0000

    投稿詳細確認機能を追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
   def index
     @posts = Post.all
   end
@@ -40,6 +41,10 @@ class PostsController < ApplicationController
   private
   def post_params
     params.require(:post).permit(:content)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 
 end
